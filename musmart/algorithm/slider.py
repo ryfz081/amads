8b4a3de7517921cf6413abefc7bb617f8141b63e
Peter Harrison <pmch2@cam.ac.uk>
2024-12-07 10:31:43 +0000

    Refactoring chordify

--- musmart/algorithm/slider.py
+++ musmart/algorithm/slider.py
@@ -3,44 +3,52 @@
 Author: Peter Harrison
 """
 
+from collections import defaultdict
+from dataclasses import dataclass
 from typing import List
 from musmart.core.basics import Note, Score
 
 
 EndOfSequence = object()
 
-from dataclasses import dataclass
 
 @dataclass
 class Timepoint:
     id: int
-    time: float 
+    time: float
     note_ons: list[Note]
     note_offs: list[Note]
     sounding_notes: set[Note]
 
+    @property
+    def last_note_end(self):
+        return max(n.offset + n.dur for n in self.sounding_notes)
 
-class ScoreSlicer:
-    time_n_decimals = 6
 
-    def slice(self, score: Score):
-        timepoints = self.get_timepoints(score)
-        return self._slice(timepoints)
-    
-    def _slice(self, timepoints: List[Timepoint]):
-        raise NotImplementedError
+@dataclass
+class Timeline:
+    timepoints: List[Timepoint]
+    time_n_decimals: int
 
-    def get_timepoints(self, score: Score) -> List[Timepoint]:
-        notes = self.get_notes(score)
+    def __len__(self):
+        return len(self.timepoints)
 
-        from collections import defaultdict
+    def __getitem__(self, index: int) -> Timepoint:
+        return self.timepoints[index]
+
+    def __iter__(self):
+        return iter(self.timepoints)
+
+    @classmethod
+    def from_score(cls, score: Score, time_n_decimals: int = 6) -> "Timeline":
+        notes = cls.get_notes(score)
 
         note_ons = defaultdict(list)
         note_offs = defaultdict(list)
-        
+
         for note in notes:
-            note_on = round(note.offset, self.time_n_decimals)
-            note_off = round(note.offset + note.dur, self.time_n_decimals)
+            note_on = round(note.offset, time_n_decimals)
+            note_off = round(note.offset + note.dur, time_n_decimals)
 
             note_ons[note_on].append(note)
             note_offs[note_off].append(note)
@@ -49,7 +57,7 @@ class ScoreSlicer:
 
         timepoints = []
         sounding_notes = set()
-        
+
         for i, time in enumerate(times):
             for note in note_offs[time]:
                 sounding_notes.discard(note)
@@ -58,37 +66,82 @@ class ScoreSlicer:
                 sounding_notes.add(note)
 
             timepoints.append(Timepoint(
-                id=i, 
-                time=time, 
-                note_ons=note_ons[time], 
+                id=i,
+                time=time,
+                note_ons=note_ons[time],
                 note_offs=note_offs[time],
                 sounding_notes=sorted(list(sounding_notes), key=lambda n: n.keynum),
             ))
 
-        return timepoints
+        return cls(timepoints, time_n_decimals)
 
-    def get_notes(self, score: Score):
+    @classmethod
+    def get_notes(cls, score: Score) -> List[Note]:
         notes = list(score.flatten().collapse_parts().find_all(Note))
         notes.sort(key=lambda n: (n.offset, n.keynum))
         return notes
-    
 
-class Chordifier(ScoreSlicer):
-    def __init__(self, slice_on_note_start: bool = True, slice_on_note_end: bool = False):
-        assert slice_on_note_start or slice_on_note_end
-        self.slice_on_note_start = slice_on_note_start
-        self.slice_on_note_end = slice_on_note_end
 
-    def _slice(self, timepoints: List[Timepoint]):
-        slices = []
-        for timepoint in timepoints:
-            if (
-                (self.slice_on_note_start and len(timepoint.note_ons) > 0)
-                or (self.slice_on_note_end and len(timepoint.note_offs) > 0)
-            ):
-                # TODO: decide whether to edit the offsets of the notes
-                # I think the answer is yes
-                slices.append(timepoint.sounding_notes)
+@dataclass
+class Slice:
+    notes: List[Note]
+    original_notes: List[Note]
+    start: float
+    end: float
 
-        return slices
-        
+    def __iter__(self):
+        return iter(self.notes)
+
+    def __len__(self):
+        return len(self.notes)
+
+
+# TODO:
+def salami_slice(
+        score: Score,
+        min_slice_duration: float = 0.0,
+        include_empty_slices: bool = True,
+        slice_on_note_start: bool = True,
+        slice_on_note_end: bool = True,
+) -> List[Slice]:
+    timeline = Timeline.from_score(score)
+    slices = []
+
+    for i, timepoint in enumerate(timeline):
+        if (
+            (slice_on_note_start and len(timepoint.note_ons) > 0)
+            or (slice_on_note_end and len(timepoint.note_offs) > 0)
+        ):
+
+            assert len(timepoint.sounding_notes) > 0
+
+            try:
+                next_timepoint = timeline[i + 1]
+            except IndexError:
+                next_timepoint = None
+
+            slice_start = timepoint.time
+
+            if next_timepoint is None:
+                slice_end = timepoint.last_note_end
+            else:
+                slice_end = next_timepoint.time
+
+            slice_duration = slice_end - slice_start
+
+            if slice_duration < min_chord_duration:
+                continue
+
+            notes = [note.copy() for note in timepoint.sounding_notes]
+            for note in notes:
+                note.offset = slice_start
+                note.dur = slice_end - slice_start
+
+            slices.append(Slice(
+                notes=notes,
+                original_notes=timepoint.sounding_notes,
+                start=slice_start,
+                end=slice_end,
+            ))
+
+    return slices
--- tests/test_score_slicer.py
+++ tests/test_score_slicer.py
@@ -1,4 +1,4 @@
-from musmart.algorithm.slider import Chordifier, chordify
+from musmart.algorithm.slider import Timeline, chordify
 from musmart.io.pt_midi_import import partitura_midi_import
 from musmart.music import example
 
@@ -7,29 +7,28 @@ def test_chordify():
     midi_file = example.fullpath("midi/twochan.mid")
     score = partitura_midi_import(midi_file, ptprint=False)
 
-    timepoints = Chordifier().get_timepoints(score)
-
-    assert len([t for t in timepoints if len(t.note_ons) > 0]) == 16
+    timeline = Timeline.from_score(score)
+    assert len([t for t in timeline if len(t.note_ons) > 0]) == 16
 
     chords = chordify(score)
-    
+
     assert len(chords) == 16
     pitches = [[int(p.keynum) for p in c] for c in chords]
     assert pitches == [
-        [43, 67], 
-        [43, 64], 
-        [43, 67], 
-        [43, 65], 
-        [45, 67], 
-        [45, 64], 
-        [43, 67], 
-        [43, 71], 
-        [40, 69], 
-        [40, 71], 
-        [38, 74], 
-        [38, 71], 
-        [43, 69], 
-        [43, 67], 
-        [47, 64], 
+        [43, 67],
+        [43, 64],
+        [43, 67],
+        [43, 65],
+        [45, 67],
+        [45, 64],
+        [43, 67],
+        [43, 71],
+        [40, 69],
+        [40, 71],
+        [38, 74],
+        [38, 71],
+        [43, 69],
+        [43, 67],
+        [47, 64],
         [47, 67]
     ]
