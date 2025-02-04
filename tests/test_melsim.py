752551e974fe8afd18507ca0d7918d64d19c38f2
Peter Harrison <pmch2@cam.ac.uk>
2025-02-04 13:51:33 +0000

    Install dependencies within pytest

--- tests/test_melsim.py
+++ tests/test_melsim.py
@@ -1,4 +1,18 @@
-def test_melsim_import():
+import os
+
+import pytest
+
+from amads.melody.similarity.melsim import check_r_packages_installed
+
+
+@pytest.fixture(scope="session")
+def installed_melsim_dependencies():
+    on_ci = os.environ.get("CI") is not None
+    install_missing = on_ci
+    check_r_packages_installed(install_missing=install_missing)
+
+
+def test_melsim_import(installed_melsim_dependencies):
     """Test that melsim can be imported."""
     from amads.melody.similarity.melsim import get_similarity
 
