99f0db6c07212fd5e960c2a0dfa1c2fc0f82050b
isebenius <isaac.sebenius@gmail.com>
2025-02-06 17:46:49 +0000

    Create working MelodyIntervalTokenizer

--- amads/expectation/tokenizer.py
+++ amads/expectation/tokenizer.py
@@ -1,18 +1,18 @@
 from typing import List
 from amads.core.basics import Score
-from amads.pitch import ismonophonic
-from amads.expectation.token import Token
-
+from amads.pitch.ismonophonic import ismonophonic
+from amads.core.basics import Note
+from abc import ABC, abstractmethod
 
 class Token:
     def __init__(self, value: int):
         self.value = value
 
-
-class Tokenizer:
+class Tokenizer(ABC):
+    @abstractmethod    
     def tokenize(self, x) -> List[Token]:
-        raise NotImplementedError
-
+        """All tokenizer subclasses must implement this method"""
+        pass
 
 class MelodyIntervalTokenizer(Tokenizer):
     """
@@ -22,15 +22,15 @@ class MelodyIntervalTokenizer(Tokenizer):
         assert isinstance(x, Score)
         assert ismonophonic(x)
 
+        flat_score = x.flatten(collapse=True)
+        tones = list(flat_score.find_all(Note))
         tokens = []
-
-        for prev_note, current_note in zip(x.notes[:-1], x.notes[1:]):
+        
+        for prev_note, current_note in zip(tones[:-1], tones[1:]):
             interval = current_note.keynum - prev_note.keynum
             tokens.append(Token(interval))
 
         return tokens
-
-
 
 class AudioTokenizer(Tokenizer):
     pass
