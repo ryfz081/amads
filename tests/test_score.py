86e48dfc149878e5971f8003ae528fb0841ceaab
Peter Harrison <pmch2@cam.ac.uk>
2025-03-20 10:27:47 +0000

    Implement test_copy_score

--- tests/test_score.py
+++ tests/test_score.py
@@ -1,6 +1,6 @@
 import pytest
 
-from amads.core.basics import Score
+from amads.core.basics import Note, Part, Score
 
 
 def test_from_melody_overlapping_notes():
@@ -23,3 +23,19 @@ def test_from_melody_empty_pitches():
     assert score.duration == 0.0
     assert len(score.content) == 1  # should have one empty part
     assert len(score.content[0].content) == 0  # part should have no notes
+
+
+def test_copy_score():
+    score = Score.from_melody(pitches=[60, 62, 64], durations=1.0)
+    copied_score = score.copy()
+
+    copied_note = copied_score.content[0].content[0]
+    assert isinstance(copied_note, Note)
+
+    copied_part = copied_score.content[0]
+    assert isinstance(copied_part, Part)
+
+    assert copied_note.parent == copied_part
+    assert copied_part.parent == copied_score
+
+    assert copied_part.parent != score
