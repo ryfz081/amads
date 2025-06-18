4154a2ec0346ad0fdc715d3f69aaccffea83177c
ryfz081 <ryanzhangxyz@gmail.com>
2025-06-18 19:27:14 -0400

    update concur

--- amads/time/concur.py
+++ amads/time/concur.py
@@ -2,36 +2,41 @@ import numpy as np
 from ..core.basics import Note, Score
 from ..algorithms.nnotes import nnotes
 
-def concur(score: Score):
+def concur(score: Score, threshold: float = None):
     """
 
     
     Args:
         score (Score): The musical score to analyze
-        float (Threshold): (optional) value for threshold for concurrent
+        threshold (float): (optional) value for threshold for concurrent
                             onsets default value is +/- 0.2 beats
 
     Returns:
-        int (c): integer displaying the proportion of concurrent onsets
+        c (float): number representing the proportion of concurrent onsets
         
         
     Example:
         concur(score, 0.25)
     """
     
-    a = nnotes(Score)
+    a = nnotes(score)
     #if score is empty, return
     if a == 0: return; end
     
     #if threshold is not provided, set it to 0.2
-    if threshold is None: threshold = 0.2;
+    if threshold is None: threshold = 0.2
 
     #get unique onset times
     
-    onsets = [n._convert_to_quarters.onset for n in Score.find_all(Note)]
-    u = np.unique(onsets);
+    all_onsets = np.array([n.onset for n in score.find_all(Note)])
+    unique_onsets = np.unique(all_onsets)
+    
+    s = 0
+    for z in unique_onsets:
+        num_concurrent = np.sum((all_onsets >= z - threshold) & (all_onsets <= z + threshold)) - 1
+        s += num_concurrent
 
     #calculate the proportion of concurrent onsets
-    c = 1 - len(u)/len(onsets); 
-    return c;
-    
+    c = s/a
+
+    return float(c)
